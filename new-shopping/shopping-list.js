2a9034e09719b1c96bb52d7d49afb7e74cf0fb73
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-05-04 16:25:33 +0530

    remove item and clear all functionality added

--- new-shopping/shopping-list.js
+++ new-shopping/shopping-list.js
@@ -2,6 +2,9 @@
 const inputEl = document.getElementById("item-input");
 const formEl = document.getElementById("item-form");
 const itemListEl = document.getElementById("item-list");
+const removeItemBtn = document.querySelectorAll(".remove-item");
+const clearBtn = document.getElementById("clear");
+const filterEl = document.getElementById("filter");
 // Helper Functions
 
 const createIcon = (classes) => {
@@ -34,7 +37,41 @@ const onSubmit = (e) => {
   const childBtn = createBtn("remove-item btn-link text-red");
   li.appendChild(childBtn);
   itemListEl.appendChild(li);
+  inputEl.value = "";
+  checkUi();
+};
+
+const removeItem = (e) => {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure to delete this item?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+
+    checkUi();
+  }
+};
+
+const clearAll = (e) => {
+  while (itemListEl.firstChild) {
+    itemListEl.removeChild(itemListEl.firstChild);
+  }
+  checkUi();
+};
+
+const checkUi = () => {
+  const items = itemListEl.querySelectorAll("li");
+
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    filterEl.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    filterEl.style.display = "block";
+  }
 };
 
 // Event Listeners
 formEl.addEventListener("submit", onSubmit);
+itemListEl.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearAll);
+checkUi();
