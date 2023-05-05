8154b80ebeb9e40692bb43955d8910f9234df1cd
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-05-05 23:22:58 +0530

    challenge updated

--- working_with_arrays/challenges.js
+++ working_with_arrays/challenges.js
@@ -1,13 +1,20 @@
 const juliaArr = [3, 5, 2, 12, 7];
 const kateArr = [4, 1, 15, 8, 3];
+const juliaArr2 = [9, 16, 6, 8, 3];
+const kateArr2 = [10, 5, 6, 1, 4];
 
-const juliaCorrectedArr = juliaArr.slice(1, 4);
+const dogsChallenge = (dogs1, dogs2) => {
+  const correctedDogs1 = dogs1.slice(1, 3);
+  const dogs = correctedDogs1.concat(dogs2);
 
-const resultantArr = juliaCorrectedArr.concat(kateArr);
-resultantArr.forEach((dog, index) => {
-  if (dog >= 3) {
-    console.log(`Dog number ${index + 1} is an adult, and is ${dog} years old`);
-  } else {
-    console.log(`Dog number ${index + 1} is still a puppy 🐶`);
-  }
-});
+  console.log(dogs);
+
+  dogs.forEach((dog, i) =>
+    dog >= 3
+      ? console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`)
+      : console.log(`Dog number ${i + 1} is still a puppy 🐶`)
+  );
+};
+
+dogsChallenge(juliaArr, kateArr);
+dogsChallenge(juliaArr2, kateArr2);
